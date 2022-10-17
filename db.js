2bc7c32bc78d0e14565c0eb30600f946bd471b07
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2022-10-17 20:05:00 +0530

    git commit -m "Fixed Issue #18 i.e Responsiveness for mobile view"

--- db.js
+++ db.js
@@ -1,13 +1,27 @@
+// const mongoose = require('mongoose')
+
+// require('dotenv').config()
+
+// const connectDB = async () => {
+//   await mongoose.connect(process.env.MONGO_URI).then(()=>{
+//     console.log('connection successful')
+//   }).catch((err)=>{
+//     console.log(err)
+//   })
+// }
+
+// module.exports = connectDB
+
 const mongoose = require('mongoose')
 
-require('dotenv').config()
-
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("connection successful")
-  }).catch((err)=>{
-    console.log(err)
-  })
+  await mongoose.connect(
+    'mongodb://localhost:27017/todolistDB',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log('Connected to MongoDB')
+    }
+  )
 }
 
 module.exports = connectDB

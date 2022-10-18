ea2d5b7933d69265e1d1b317ba09dd84fd67fb98
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2022-10-18 23:15:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,27 +1,14 @@
-// const mongoose = require('mongoose')
+ const mongoose = require('mongoose')
 
-// require('dotenv').config()
+ require('dotenv').config()
 
-// const connectDB = async () => {
-//   await mongoose.connect(process.env.MONGO_URI).then(()=>{
-//     console.log('connection successful')
-//   }).catch((err)=>{
-//     console.log(err)
-//   })
-// }
-
-// module.exports = connectDB
-
-const mongoose = require('mongoose')
-
-const connectDB = async () => {
-  await mongoose.connect(
-    'mongodb://localhost:27017/todolistDB',
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log('Connected to MongoDB')
-    }
-  )
+  const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('connection successful')
+  }).catch((err)=>{
+    console.log(err)
+ })
 }
 
 module.exports = connectDB
+
